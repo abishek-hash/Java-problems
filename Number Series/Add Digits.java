dbbe14d599fef779e1314047fc643062c40a84de
AbishekPadmanaban <abishekpadmanaban010@gmail.com>
2026-07-01 21:23:31 +0530

    Add files via upload

---
+++ Number Series/Add Digits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n >= 10) {
+            int sum = 0;
+
+            while (n > 0) {
+                int r = n % 10;
+                sum = sum + r;
+                n = n / 10;
+            }
+
+            n = sum;
+        }
+
+        System.out.println(n);
+    }
+}
