b1f120adf116a94d9f85f583f4382c3a080f6cd1
AbishekPadmanaban <abishekpadmanaban010@gmail.com>
2026-07-04 19:21:15 +0530

    Add files via upload

---
+++ Number Series/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        int r=0;
+        int m=n;
+        while(n>0){
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        if(rev==m){
+            System.out.println("Palindrome:"+m);
+            
+        }
+        else{
+            System.out.println("Not a Palindrome:"+m);
+            
+        }
+    }
+}
