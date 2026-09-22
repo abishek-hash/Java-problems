0ee72b2e638f33d0532c0059442f8376ca69bc34
AbishekPadmanaban <abishekpadmanaban010@gmail.com>
2026-09-22 21:37:53 +0530

    Add files via upload

---
+++ Arrays/Movezerolast.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Movezerolast {
+
+    public static void Movezerolast(int n, int arr[]) {
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Movezerolast(n, arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
