ea1bccafc1f0423a38ca1a0d6f10fbffa73eabba
AbishekPadmanaban <abishekpadmanaban010@gmail.com>
2026-09-24 22:22:22 +0530

    Add files via upload

---
+++ Arrays/Removeduplicates.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        
+        int index = 0;
+        for(int i = 1; i < n; i++){
+            if(arr[i] != arr[i-1]){
+                index++;
+                arr[index] = arr[i];
+            }
+        }
+        
+        for(int i = 0; i <= index; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
