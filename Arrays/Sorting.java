389721b2af43c60e3d0c361dcf2d3c59159e667c
AbishekPadmanaban <abishekpadmanaban010@gmail.com>
2026-09-25 22:55:36 +0530

    Add files via upload

---
+++ Arrays/Sorting.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void Bubblesorting(int arr[],int n){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    
+    }
+
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
+    int arr[]=new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+}
+Bubblesorting(arr,n);
+
+for(int i=0;i<n;i++){
+    System.out.println(arr[i]+" ");
+        }
+    }
+}
+
+
+
+
+
